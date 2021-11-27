bcafe5ebd1a6874feec2bdb7e2ede746dca8c26b
Evgeny Kyselgov <kyselgov@gmail.com>
2021-11-27 19:46:11 +0200

    add ListView controls

--- TOPS/L2/L2/MainForm.Designer.cs
+++ TOPS/L2/L2/MainForm.Designer.cs
@@ -30,6 +30,13 @@ namespace L2
         private void InitializeComponent()
         {
             this.m_runButton = new System.Windows.Forms.Button();
+            this.m_listView = new System.Windows.Forms.ListView();
+            this.m_methodColumn = new System.Windows.Forms.ColumnHeader();
+            this.m_attemptsColumn = new System.Windows.Forms.ColumnHeader();
+            this.m_avgTimeColumn = new System.Windows.Forms.ColumnHeader();
+            this.m_stepsColumnt = new System.Windows.Forms.ColumnHeader();
+            this.m_minFColumn = new System.Windows.Forms.ColumnHeader();
+            this.m_minXColumn = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // m_runButton
@@ -42,14 +49,57 @@ namespace L2
             this.m_runButton.UseVisualStyleBackColor = true;
             this.m_runButton.Click += new System.EventHandler(this.OnRun);
             // 
+            // m_listView
+            // 
+            this.m_listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.m_methodColumn,
+            this.m_attemptsColumn,
+            this.m_avgTimeColumn,
+            this.m_stepsColumnt,
+            this.m_minFColumn,
+            this.m_minXColumn});
+            this.m_listView.HideSelection = false;
+            this.m_listView.Location = new System.Drawing.Point(12, 208);
+            this.m_listView.Name = "m_listView";
+            this.m_listView.Size = new System.Drawing.Size(776, 195);
+            this.m_listView.TabIndex = 1;
+            this.m_listView.UseCompatibleStateImageBehavior = false;
+            this.m_listView.View = System.Windows.Forms.View.Details;
+            // 
+            // m_methodColumn
+            // 
+            this.m_methodColumn.Text = "Method";
+            // 
+            // m_attemptsColumn
+            // 
+            this.m_attemptsColumn.Text = "Attempts";
+            // 
+            // m_avgTimeColumn
+            // 
+            this.m_avgTimeColumn.Text = "Time.avg (ms)";
+            // 
+            // m_stepsColumnt
+            // 
+            this.m_stepsColumnt.Text = "Steps";
+            // 
+            // m_minFColumn
+            // 
+            this.m_minFColumn.Text = "F(min(x))";
+            // 
+            // m_minXColumn
+            // 
+            this.m_minXColumn.Text = "min(X)";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.m_listView);
             this.Controls.Add(this.m_runButton);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Minimizers comparision";
+            this.Load += new System.EventHandler(this.OnLoad);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +107,13 @@ namespace L2
         #endregion
 
         private System.Windows.Forms.Button m_runButton;
+        private System.Windows.Forms.ListView m_listView;
+        private System.Windows.Forms.ColumnHeader m_methodColumn;
+        private System.Windows.Forms.ColumnHeader m_attemptsColumn;
+        private System.Windows.Forms.ColumnHeader m_avgTimeColumn;
+        private System.Windows.Forms.ColumnHeader m_stepsColumnt;
+        private System.Windows.Forms.ColumnHeader m_minFColumn;
+        private System.Windows.Forms.ColumnHeader m_minXColumn;
     }
 }
 
--- TOPS/L2/L2/MainForm.cs
+++ TOPS/L2/L2/MainForm.cs
@@ -31,6 +31,27 @@ namespace L2
             InitializeComponent();
         }
 
+        void ColdStart()
+        {
+            // do dummy calculation at first call (avoid cold start problem)
+            var f = new RosenbrockFunction();
+            var x0 = new double[] { -1.2, 1.0 };
+
+            var testPlan = new TestPlan()
+            {
+                Experiments = new Test[]
+                {
+                    new Test(f, x0, new FunctionOptimization(f) { Method = FunctionOptimization.MinMethod.Simplex }),
+                    new Test(f, x0, new FunctionOptimization(f) { Method = FunctionOptimization.MinMethod.Gradient }),
+                    new Test(f, x0, new FunctionOptimization(f) { Method = FunctionOptimization.MinMethod.Newton }),
+                    new Test(f, x0, new FunctionOptimization(f) { Method = FunctionOptimization.MinMethod.LevenbergMarquardt }),
+                    new Test(f, x0, new FunctionOptimization(f) { Method = FunctionOptimization.MinMethod.BFGS }),
+                }
+            };
+
+            testPlan.Run();
+        }
+
         void Run()
         {
             TestPlan.Run();
@@ -38,12 +59,41 @@ namespace L2
 
         void UpdateControls()
         {
+            m_listView.BeginUpdate();
+            m_listView.Items.Clear();
+
+            foreach(var e in TestPlan.Experiments)
+            {
+                var minX = string.Join("; ", e.BestResult.MinX.Select(x => $"{x:F3}"));
+
+                m_listView.Items.Add(new ListViewItem(new string[] { $"{e.Minimizer.Method}",
+                                                                     $"{e.SuccededAttempts}",
+                                                                     $"{e.AvgTime_ms:F3}",
+                                                                     $"{e.BestResult.Steps}",
+                                                                     $"{e.BestResult.MinF:G4}",
+                                                                     minX,
+                                                                    }));
+            }
+
+            m_methodColumn.Width = -2;
+            m_attemptsColumn.Width = -2;
+            m_avgTimeColumn.Width = -2;
+            m_stepsColumnt.Width = -2;
+            m_minFColumn.Width = -2;
+            m_minXColumn.Width = -2;
+
+            m_listView.EndUpdate();
         }
 
         void OnRun(object sender, EventArgs e)
         {
             Run();
             UpdateControls();
+        }
+
+        void OnLoad(object sender, EventArgs e)
+        {
+            ColdStart();
         }
     }
 }
