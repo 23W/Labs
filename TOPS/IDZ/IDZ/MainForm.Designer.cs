bb208e22f564983a7f8f03f825fb9bfdb164f95a
Evgeny Kyselgov <kyselgov@gmail.com>
2022-01-14 15:18:21 +0200

    draw function

--- TOPS/IDZ/IDZ/MainForm.Designer.cs
+++ TOPS/IDZ/IDZ/MainForm.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.m_runButton = new System.Windows.Forms.Button();
+            this.m_plotView = new OxyPlot.WindowsForms.PlotView();
+            this.SuspendLayout();
+            // 
+            // m_runButton
+            // 
+            this.m_runButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_runButton.Location = new System.Drawing.Point(1048, 745);
+            this.m_runButton.Name = "m_runButton";
+            this.m_runButton.Size = new System.Drawing.Size(94, 29);
+            this.m_runButton.TabIndex = 0;
+            this.m_runButton.Text = "Run";
+            this.m_runButton.UseVisualStyleBackColor = true;
+            this.m_runButton.Click += new System.EventHandler(this.OnRun);
+            // 
+            // m_plotView
+            // 
+            this.m_plotView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_plotView.BackColor = System.Drawing.Color.White;
+            this.m_plotView.Location = new System.Drawing.Point(172, 12);
+            this.m_plotView.Name = "m_plotView";
+            this.m_plotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.m_plotView.Size = new System.Drawing.Size(970, 727);
+            this.m_plotView.TabIndex = 1;
+            this.m_plotView.Text = "plotView";
+            this.m_plotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.m_plotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.m_plotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1154, 786);
+            this.Controls.Add(this.m_plotView);
+            this.Controls.Add(this.m_runButton);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.Text = "IDZ";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button m_runButton;
+        private OxyPlot.WindowsForms.PlotView m_plotView;
     }
 }
--- TOPS/IDZ/IDZ/MainForm.cs
+++ TOPS/IDZ/IDZ/MainForm.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Legends;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,5 +21,79 @@ namespace IDZ
             InitializeComponent();
         }
 
+        void Run()
+        {
+
+        }
+
+        void UpdateControls()
+        {
+            // plot view
+            {
+                var fn = new Function();
+                double x0 = fn.LowerBound;
+                double x1 = fn.UpperBound;
+
+                var plotModel = new PlotModel()
+                {
+                    Background = OxyColors.White,
+                    PlotAreaBorderColor = OxyColors.Black
+                };
+
+                // axes
+                {
+                    var xAxis = new LinearAxis()
+                    {
+                        Title = "X",
+                        Minimum = x0,
+                        Maximum = x1,
+                        Position = AxisPosition.Bottom,
+                        IsPanEnabled = false,
+                        IsZoomEnabled = false,
+                        MajorGridlineStyle = LineStyle.Solid,
+                        MinorGridlineStyle = LineStyle.Dot
+                    };
+
+                    var yAxis = new LinearAxis()
+                    {
+                        Title = "Y",
+                        Position = AxisPosition.Left,
+                        IsPanEnabled = false,
+                        IsZoomEnabled = false,
+                        MajorGridlineStyle = LineStyle.Solid,
+                        MinorGridlineStyle = LineStyle.Dot
+                    };
+
+                    plotModel.Axes.Add(xAxis);
+                    plotModel.Axes.Add(yAxis);
+                }
+
+                // legend
+                {
+                    var l = new Legend()
+                    {
+                        LegendPlacement = LegendPlacement.Outside
+                    };
+
+                    plotModel.Legends.Add(l);
+                }
+
+                // functions
+                {
+                    const int fnSteps = 1000;
+
+                    var fns = new FunctionSeries(x => fn.CalcValue(x), x0, x1, (x1 - x0) / fnSteps, "f(x)");
+                    plotModel.Series.Add(fns);
+                }
+
+                m_plotView.Model = plotModel;
+            }
+        }
+
+        private void OnRun(object sender, EventArgs e)
+        {
+            Run();
+            UpdateControls();
+        }
     }
 }
