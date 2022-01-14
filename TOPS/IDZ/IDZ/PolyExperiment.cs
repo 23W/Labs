cd9339e049f5fc617a3b2b63178cf4276e6f9532
Evgeny Kyselgov <kyselgov@gmail.com>
2022-01-14 20:02:52 +0200

    calc std deviation

--- TOPS/IDZ/IDZ/PolyExperiment.cs
+++ TOPS/IDZ/IDZ/PolyExperiment.cs
@@ -14,13 +14,26 @@ namespace IDZ
         internal int TableRank { get; set; } = 4;
         internal ITableFunction? TableFunction { get; private set; } = default(ITableFunction);
         internal IFunction? G { get; private set; } = default(IFunction);
+        internal double StdDev { get; private set; }
+        internal int StdDevQuality => 1000;
         internal OptimizationResult Optimum { get; private set; } = default(OptimizationResult);
 
         internal void Run()
         {
+            // polynom interpolation
             TableFunction = new TableFunction(F, X0, X1, TableRank);
             G = new PolynomialInterpolation(TableFunction);
 
+            // standard deviation of intrepolation
+            var stdTableFunction = new TableFunction(F, X0, X1, StdDevQuality);
+            var cumulativeError = 0.0;
+            for (var r = 0; r < StdDevQuality; r++)
+            {
+                cumulativeError += Math.Pow(stdTableFunction.Y[r] - G.CalcValue(stdTableFunction.X[r]), 2);
+            }
+            StdDev = Math.Sqrt(cumulativeError / StdDevQuality);
+
+            // maximum value
             var minimizer = new GoldenRatioMinimizer() { F = F, X0 = X0, X1 = X1, Tolerance = 1e-8 };
             Optimum = minimizer.CalcMaximum();
         }
