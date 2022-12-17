31771c027ac9f669c9db9acbe304294bc7ca6e4e
Evgen Kyselgov <kyselgov@gmail.com>
2022-12-18 01:06:03 +0200

    implement simpl greedy algorithm

---
+++ GTandSD/LB2/LB2/Greedy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2
+{
+    internal static class Greedy
+    {
+        internal static int[,] Solve(int[,] effort, int[] sourceConstrain, int[] destinationConstraing)
+        {
+            var n = effort.GetLength(1); // columns
+            var m = effort.GetLength(0); // rows
+
+            Debug.Assert(m == sourceConstrain.Length);
+            Debug.Assert(n == destinationConstraing.Length);
+            Debug.Assert(sourceConstrain.Sum() == destinationConstraing.Sum());
+
+            const int unused = int.MinValue;
+
+            var res = new int[m, n];
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    res[j, i] = unused;
+                }
+            }
+
+            bool stop = false;
+            do
+            {
+                int minE = int.MaxValue;
+                int minN = unused, minM = unused;
+
+                for (var i = 0; i < n; i++)
+                {
+                    for (var j = 0; j < m; j++)
+                    {
+                        if (res[j, i] == unused && 
+                            effort[j, i] < minE)
+                        {
+                            minE = effort[j, i];
+                            minN = i;
+                            minM = j;
+                        }
+                    }
+                }
+
+                stop = minN == unused ||
+                       minM == unused;
+                if (!stop)
+                {
+                    if (sourceConstrain[minM] < destinationConstraing[minN])
+                    {
+                        res[minM, minN] = sourceConstrain[minM];
+                    }
+                    else
+                    {
+                        res[minM, minN] = destinationConstraing[minN];
+                    }
+
+                    sourceConstrain[minM] -= res[minM, minN];
+                    destinationConstraing[minN] -= res[minM, minN];
+                }
+            }
+            while (!stop);
+
+            return res;
+        }
+    }
+}
---
+++ GTandSD/LB2/LB2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using LB2;
+
+Console.WriteLine("Greedy Algorithm");
+
+var efforts = new int[,] { { 4000, 7200 }, { 2800, 6400 }, { 4800, 8000 } };
+var sourceConstrains = new int[] { 1000, 1500, 1200 };
+var destinationConstrains = new int[] { 2300, 1400 };
+
+var res = Greedy.Solve((int[,])efforts.Clone(),
+                       (int[])sourceConstrains.Clone(),
+                       (int[])destinationConstrains.Clone());
