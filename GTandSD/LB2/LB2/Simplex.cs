04585edf99ccb378712c2d508f484d0d7c53baa7
Evgen Kyselgov <kyselgov@gmail.com>
2022-12-18 12:05:02 +0200

    remove unused dependencies

--- GTandSD/LB2/LB2/Simplex.cs
+++ GTandSD/LB2/LB2/Simplex.cs
@@ -1,10 +1,5 @@
 ﻿using CenterSpace.NMath.Core;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LB2
 {
