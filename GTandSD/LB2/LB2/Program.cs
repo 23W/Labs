1c20c19127de4b2dec50c77a6705603cb8d49ee0
Evgen Kyselgov <kyselgov@gmail.com>
2022-12-18 09:44:24 +0200

    add Simplex algorithm

--- GTandSD/LB2/LB2/Program.cs
+++ GTandSD/LB2/LB2/Program.cs
@@ -1,13 +1,16 @@
-﻿
-
-using LB2;
+﻿using LB2;
 
 Console.WriteLine("Greedy Algorithm");
 
+// Task data
 var efforts = new int[,] { { 4000, 7200 }, { 2800, 6400 }, { 4800, 8000 } };
 var sourceConstrains = new int[] { 1000, 1500, 1200 };
 var destinationConstrains = new int[] { 2300, 1400 };
 
-var res = Greedy.Solve((int[,])efforts.Clone(),
-                       (int[])sourceConstrains.Clone(),
-                       (int[])destinationConstrains.Clone());
+var greedyRes = Greedy.Solve((int[,])efforts.Clone(),
+                             (int[])sourceConstrains.Clone(),
+                             (int[])destinationConstrains.Clone());
+
+var simplexRes = Simplex.Solve((int[,])efforts.Clone(),
+                               (int[])sourceConstrains.Clone(),
+                               (int[])destinationConstrains.Clone());
---
+++ GTandSD/LB2/LB2/Simplex.cs
@@ -0,0 +1,88 @@
+﻿using CenterSpace.NMath.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2
+{
+    // Solves LP transport task by Simplex algorithm (NMath implementation)
+    internal static class Simplex
+    {
+        internal static int[,] Solve(int[,] effort, int[] sourceConstrain, int[] destinationConstraing)
+        {
+            var n = effort.GetLength(1); // columns
+            var m = effort.GetLength(0); // rows
+
+            Debug.Assert(m == sourceConstrain.Length);
+            Debug.Assert(n == destinationConstraing.Length);
+            Debug.Assert(sourceConstrain.Sum() == destinationConstraing.Sum());
+
+            var c = new DoubleVector(m * n);
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    c[j * n + i] = effort[j, i];
+                }
+            }
+
+            var lp = new MixedIntegerLinearProgrammingProblem(c);
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    lp.AddLowerBound(j * n + i, 0);
+                    lp.SetIntegrality(j * n + i, true);
+                }
+            }
+
+            for (var j = 0; j < m; j++)
+            {
+                var sc = new DoubleVector(m * n);
+                sc.Clear();
+
+                for (var i = 0; i < n; i++)
+                {
+                    sc[j * n + i] = 1;
+                }
+
+                lp.AddUpperBoundConstraint(sc, sourceConstrain[j]);
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                var dc = new DoubleVector(m * n);
+                dc.Clear();
+
+                for (var j = 0; j < m; j++)
+                {
+                    dc[j * n + i] = 1;
+                }
+
+                lp.AddEqualityConstraint(dc, destinationConstraing[i]);
+            }
+
+            var solver = new DualSimplexSolverORTools();
+            solver.Solve(lp, true);
+
+            Debug.Assert(solver.Result == ConstrainedOptimizerORTools.SolveResult.Optimal);
+            Debug.Assert(solver.OptimalX.Length == m * n);
+
+            var optimalX = solver.OptimalX.ToArray();
+            var res = new int[m, n];
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    res[j, i] = (int)optimalX[j * n + i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
